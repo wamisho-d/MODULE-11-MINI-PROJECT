500df31ec709e377c122ece65bc97d2535d9719a
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2024-09-14 14:03:06 -0400

    Update # 1 deletecustomer.js

---
+++ # 1 deletecustomer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { deleteCustomer } from '../api/customerAPI';
+
+const deleteCustomer = async (customerId) => {
+    await deleteCustomer(customerId);
+};
+
+export default deleteCustomer;
